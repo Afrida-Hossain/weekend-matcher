c9ed53ace1c8395faad4612b0f1caf11429e68ea
Afrida-Hossain <afrida.hossain.18@gmail.com>
2021-08-31 21:48:09 +0600

    removed variable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Form } from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private a = 5;
+ 
   constructor() { }
 
   ngOnInit(): void {
